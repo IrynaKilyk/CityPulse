a0482b8d5716b7607804ef291aec6373acde7c4a
Iryna Kilyk <kilukiryna@gmail.com>
2026-08-29 17:39:59 +0300

    refactor(core): extract target locations and common db logic

--- src/db.py
+++ src/db.py
@@ -15,4 +15,19 @@ def get_connection():
             gssencmode="disable"
         )
         return conn
+
+def get_or_create_city(conn, city_name: str, lat: float, lon: float, country: str, region: str):
+    with conn.cursor() as cur:
+        cur.execute("""
+    INSERT INTO cities(city_name, latitude, longitude, country, region) 
+    VALUES(%s, %s, %s, %s, %s)
+    ON CONFLICT (city_name) DO NOTHING;""",
+        (city_name, lat, lon , country, region)
+    )
+    conn.commit()
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT id from cities WHERE city_name = %s", (city_name,))
+        number_id =cur.fetchone()[0]
+    return number_id
   
